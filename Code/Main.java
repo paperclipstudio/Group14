8fdc95d6f47f2e874d8ecd36af28275bcf13fe67
George Sanger <paperclipstudio@gmail.com>
2020-11-10 13:12:54 +0000

    Running main class

--- Code/Main.java
+++ Code/Main.java
@@ -1,5 +1,6 @@
 
-public static void main(String[] args) {
-    System.out.println("Hello World");
-    return 
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
