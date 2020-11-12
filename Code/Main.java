3d55206331ef2d95813edd8609a1d787559f8e86
George Sanger <paperclipstudio@gmail.com>
2020-11-12 15:35:46 +0000

    Added gameScreen Class

--- Code/Main.java
+++ Code/Main.java
@@ -4,8 +4,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         System.out.println("Hello World");
+        // You can use the line below to test out your own screens
+        //primaryStage.setScene(GameScreen.makeWindow());
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
