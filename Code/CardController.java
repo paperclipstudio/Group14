c5e04d4e709f3b6c80de4e74acbfb6a8e08da857
George Sanger <paperclipstudio@gmail.com>
2020-11-13 16:37:23 +0000

    Made a nicer GameScreen and Card object

---
+++ Code/CardController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardController implements Initializable {
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+}
--- Code/Main.java
+++ Code/Main.java
@@ -1,4 +1,7 @@
 import  javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -6,7 +9,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         System.out.println("Hello World");
         // You can use the line below to test out your own screens
-        primaryStage.setScene(GameScreen.makeWindow());
+        Parent root = FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
