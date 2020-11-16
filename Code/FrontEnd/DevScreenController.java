a640a1b29c9d9020b1ee6605cf75757b36f4e286
George Sanger <paperclipstudio@gmail.com>
2020-11-16 14:09:45 +0000

    Added counter button to show javaFX working
    Signed-off-by: George Sanger <paperclipstudio@gmail.com>

--- Code/FrontEnd/DevScreenController.java
+++ Code/FrontEnd/DevScreenController.java
@@ -1,6 +1,8 @@
 package FrontEnd;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,8 +13,23 @@ import java.util.ResourceBundle;
  */
 public class DevScreenController implements Initializable {
 
+	@FXML
+	private Button counterButton;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("Dev Screen created");
+	}
+
+	public void incCounter() {
+		String text = counterButton.getText();
+		if (text.equals("Add one to count")) {
+			counterButton.setText("1");
+		} else {
+			int number = Integer.parseInt(text);
+			number++;
+			counterButton.setText(Integer.toString(number));
+		}
+
 	}
 }
