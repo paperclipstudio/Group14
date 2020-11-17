9ad4e579dc3ca20338c7b808be40886e0ff03633
paperclipstudio <paperclipstudio@gmail.com>
2020-11-17 16:00:37 +0000

    Added settings window

--- Code/Main.java
+++ Code/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
         System.out.println("Hello World");
         //Parent root = null;
         // You can use the line below to test out your own screens
-        Parent root = FXMLLoader.load(getClass().getResource("FrontEnd\\DevScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("FrontEnd\\MenuScreen.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setFullScreen(false);
         primaryStage.setScene(scene);
