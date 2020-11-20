588439f4aa0e6bedf659ad9952ccbf2259949289
David Langmaid <drlangmaid@gmail.com>
2020-11-20 11:41:44 +0000

    added imports

---
+++ Code/BackEnd/GameSave.java
@@ -0,0 +1,8 @@
+package BackEnd;
+/**
+ * This class records each players decisions.
+ * We can then read this file and put it into gameLogic to load the game.
+ * @author David Langmaid
+ */
+public class GameSave {
+}
