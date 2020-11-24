2f9cc5c4a6e8a9a6d3824271f3285bc19a87f32d
paperclipstudio <paperclipstudio@gmail.com>
2020-11-24 22:21:34 +0000

    Added javadoc

--- Code/FrontEnd/WindowLoader.java
+++ Code/FrontEnd/WindowLoader.java
@@ -1,13 +1,12 @@
 package FrontEnd;
 
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /***
  * Used to load new windows
@@ -24,14 +23,24 @@ public class WindowLoader {
 	 * @param window any Node object on the stage that you wish control.
 	 */
 	public WindowLoader(Node window) {
-		this.w = (Stage) window.getScene().getWindow();
+		w = (Stage) window.getScene().getWindow();
 	}
 
+	/**
+	 * Set if the window should be fullscreen or not
+	 *
+	 * @param fullscreen true for fullscreen, false for not
+	 */
 	public static void setFullScreen(boolean fullscreen) {
 		isFullscreen = fullscreen;
 		w.setFullScreen(isFullscreen);
 	}
 
+	/**
+	 * Returns if the screen is set to be fullscreen
+	 *
+	 * @return true for fullscreen false for not
+	 */
 	public static boolean getIsFullScreen() {
 		return isFullscreen;
 	}
@@ -39,12 +48,13 @@ public class WindowLoader {
 	/***
 	 * swaps the scene for the given scene. Window should be the scene file name
 	 * i.e. to swap to MenuScreen.fxml use "MenuScreen"
+	 * 
 	 * @param window scene name
 	 */
 	public void load(String window) {
 		Parent root = null;
 		try {
-			root = FXMLLoader.load(getClass().getClassLoader().getResource(fileLocation + window + ".fxml"));
+			root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource(fileLocation + window + ".fxml")));
 		} catch (IOException e) {
 			System.out.print(e.getLocalizedMessage());
 			e.printStackTrace();
@@ -57,6 +67,12 @@ public class WindowLoader {
 		w.setFullScreen(isFullscreen);
 	}
 
+	/**
+	 * Set a new resolution for the window
+	 *
+	 * @param width  the new width the window should be in pixels
+	 * @param height the new height the window should be in pixels
+	 */
 	public static void updateResolution(int width, int height) {
 		w.setWidth(width);
 		w.setHeight(height);
