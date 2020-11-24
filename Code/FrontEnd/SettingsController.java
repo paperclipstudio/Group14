a0cff3e4aa23e88bd4de648601a76de2773b8fd3
paperclipstudio <paperclipstudio@gmail.com>
2020-11-24 11:38:38 +0000

    debug mode added

--- Code/FrontEnd/SettingsController.java
+++ Code/FrontEnd/SettingsController.java
@@ -2,7 +2,14 @@ package FrontEnd;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,11 +23,44 @@ import java.util.ResourceBundle;
  */
 public class SettingsController implements Initializable {
 	@FXML
+	private Parent root;
+	@FXML
 	private Button backButton;
+	@FXML
+	private CheckBox fullscreen;
+	@FXML
+	private Slider sound;
+	@FXML
+	private ChoiceBox<String> resolution;
+	final static private String[] RESOLUTIONS = new String[] {"800x400", "1600x800"};
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		Window ps = root.getScene().getWindow();
+		resolution.getItems().add("1600x800");
+		resolution.getItems().add("800x400");
+		resolution.getSelectionModel().selectFirst();
+
 
 	}
+
+	String keyHistory = "";
+	public void onKeyPress(KeyEvent k) {
+		keyHistory += k.getCharacter();
+		if (keyHistory.length() >= 8) {
+			keyHistory = keyHistory.substring(1, 8);
+		}
+		if (keyHistory.equals("swansea")) {
+			System.out.println("DEBUG MODE ACTIVATED");
+		};
+		if (keyHistory.equals("ducksea")) {
+			System.out.println("DEBUG MODE DEACTIVATED");
+		}
+	}
+
+	public void onChoiceBox() {
+
+	}
+
 
 	/***
 	 * Returns to menu screen
