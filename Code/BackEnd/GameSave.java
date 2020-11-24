35c34b910d8599046d8a491d6570e871cc8eb54d
David Langmaid <drlangmaid@gmail.com>
2020-11-24 18:35:11 +0000

    Updated savePlayFloorTile to no longer take rotation as a parameter

--- Code/BackEnd/GameSave.java
+++ Code/BackEnd/GameSave.java
@@ -22,10 +22,10 @@ public class GameSave {
         Files.write(GAME_SAVE_FILE.toPath(),
                 (" " + location.getX() + " " + location.getY()).getBytes(), StandardOpenOption.APPEND);
     }
-    public static void savePlayFloorTile(Coordinate slideLocations, FloorTile tile, Rotation rotation) throws IOException {
+    public static void savePlayFloorTile(Coordinate slideLocations, FloorTile tile) throws IOException {
         Files.write(GAME_SAVE_FILE.toPath(),
                 ("\n" + slideLocations.getX() + " " + slideLocations.getY() + " "
-                        + tile.getType() + " " + rotation).getBytes(), StandardOpenOption.APPEND);
+                        + tile.getType() + " " + tile.getRotation()).getBytes(), StandardOpenOption.APPEND);
     }
     public static void savePlayActionTile(Coordinate location, ActionTile tile) throws IOException {
         Files.write(GAME_SAVE_FILE.toPath(),
