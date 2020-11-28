a51d26eba1587b38fbfa54e0d6d0e077c047f6b3
paracuellos12 <72559697+paracuellos12@users.noreply.github.com>
2020-11-28 04:31:01 +0000

    Start Screen

---
+++ Code/FrontEnd/StartScreenController.java
@@ -0,0 +1,38 @@
+package FrontEnd;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class StartScreenController implements Initializable {
+    @FXML
+    private Button newGameButton;
+
+    private WindowLoader wl;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void onQuitButton() {
+        Platform.exit();;
+    }
+
+    public void onNewGame() {
+        wl = new WindowLoader(newGameButton);
+        wl.load("GameSetup");
+    }
+
+    public void OnKeyPressed(KeyEvent keyEvent) {
+        wl = new WindowLoader(newGameButton);
+        wl.load("MenuScreen");
+    }
+}
