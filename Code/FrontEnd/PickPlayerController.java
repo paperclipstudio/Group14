abff2901f57280f5de30586b15ad080a6c2a4887
M16A3 <854643@swansea.ac.uk>
2020-11-29 16:57:21 +0000

    pickPlayer update

--- Code/FrontEnd/PickPlayerController.java
+++ Code/FrontEnd/PickPlayerController.java
@@ -1,94 +1,94 @@
 package FrontEnd;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.control.*;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 import BackEnd.Profile;
 
+
+/**
+ * When game start, show numbers of choiceBox to let players select player profile for different player, set these files
+ * to a array list for gameboard to use.
+ * @author zhan
+ */
 public class PickPlayerController {
 
     @FXML
-    private ListView<String> playerList;
+    public ChoiceBox<String> playerList1;
 
-    public void initialize(){
+    @FXML
+    public ChoiceBox<String> playerList2;
 
-        File file = new File("SaveData\\UserData\\");
-        String[] children = file.list();
+    @FXML
+    public ChoiceBox<String> playerList3;
 
-        if(children == null){
-            Alert alert9 = new Alert(Alert.AlertType.INFORMATION);
-            alert9.setTitle("New Player");
-            alert9.setContentText("There is no player yet! Go and create one.");
-            alert9.setHeaderText(null);
-            alert9.showAndWait();
-        }else {
-            for (String filename : children) {
-                playerList.getItems().addAll(filename);
-            }
+    @FXML
+    public ChoiceBox<String> playerList4;
+
+    @FXML
+    private Button backButton;
+
+    ArrayList<File> profiles = new ArrayList<>();
+
+    /**
+     * show player select scene
+     *      */
+     public void initialize(){
+
+        String[] players;
+        File playerLocation = new File("SaveData\\UserData\\");
+        players = playerLocation.list();
+        assert players != null;
+        for (String player : players){
+            playerList1.getItems().add(player);
+            playerList2.getItems().add(player);
+            playerList3.getItems().add(player);
+            playerList4.getItems().add(player);
         }
-        playerList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        playerList1.getSelectionModel().selectFirst();
+        playerList2.getSelectionModel().selectFirst();
+        playerList3.getSelectionModel().selectFirst();
+        playerList4.getSelectionModel().selectFirst();
 
     }
 
-    public void returnPlayers() throws IOException {
-        ObservableList<String> listOfPlayers = playerList.getSelectionModel().getSelectedItems();
-        // Create
-        // profiles[0] == profile of player 1
-        // profiles[1] == profile of player 2
-        playerList.setVisible(true);
-        Profile[] profiles = new Profile[Main.getNumberOfPlayers()];
-        Main.profiles = profiles;
-        Map<String, String> playerProfileLoaded = new HashMap<>();
-        if (listOfPlayers.size() < Main.getNumberOfPlayers()) {
-            System.out.println("Too few player selected, Pelase try again");
-        } else if (listOfPlayers.size() > Main.getNumberOfPlayers()) {
-            System.out.println("Too many players selected");
-        } else {
-        for (Object players : listOfPlayers) {
-
-            Alert alert9 = new Alert(Alert.AlertType.INFORMATION);
-            alert9.setTitle("New Player");
-            alert9.setContentText("Add player with profile " + players);
-            alert9.setHeaderText(null);
-            alert9.showAndWait();
-
-            String line;
-            BufferedReader reader = new BufferedReader(new FileReader("SaveData\\UserData\\" + players));
-
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(":", 2);
-                if (parts.length >= 2) {
-                    String getWins = parts[0];
-                    String getLosses = parts[1];
-                    playerProfileLoaded.put(getWins, getLosses);
-
-
-                    Alert alert10 = new Alert(Alert.AlertType.INFORMATION);
-                    alert10.setTitle("New Player");
-                    alert10.setContentText("This player has record " + playerProfileLoaded);
-                    alert10.setHeaderText(null);
-                    alert10.showAndWait();
-
-
-                } else {
-                    Alert alert10 = new Alert(Alert.AlertType.INFORMATION);
-                    alert10.setTitle("New Player");
-                    alert10.setContentText("This profile has wrong pattern");
-                    alert10.setHeaderText(null);
-                    alert10.showAndWait();
-                }
+    /**
+     * add the chosen player's file to the arraylist.
+     * @throws IOException
+     */
+    public void returnPlayers() {
+        try {
+            profiles.add(new File(playerList1.getValue()));
+            profiles.add(new File(playerList2.getValue()));
+            if(Main.getNumberOfPlayers() == 4){
+                profiles.add(new File(playerList3.getValue()));
+                profiles.add(new File(playerList4.getValue()));
+            }else if(Main.getNumberOfPlayers() == 3){
+                profiles.add(new File(playerList3.getValue()));
             }
-            reader.close();
+
+
+
+            // Create
+            // profiles[0] == profile of player 1
+            // profiles[1] == profile of player 2
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-}}
+
+
+    /**
+     *
+     */
+    public void onBackButton() {
+        WindowLoader wl = new WindowLoader(backButton);
+        wl.load("MenuScreen");
+    }
+}
+
