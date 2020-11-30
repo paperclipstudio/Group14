69ef23cfec65ee5a0f602de611c5af47aa905322
David Langmaid <drlangmaid@gmail.com>
2020-11-30 14:25:23 +0000

    added win screen

---
+++ Code/FrontEnd/WinScreenController.java
@@ -0,0 +1,15 @@
+package FrontEnd;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WinScreenController {
+    @FXML
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
