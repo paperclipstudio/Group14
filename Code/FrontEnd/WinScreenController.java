e1f630edc4e12f33052e0118085462ef42e98492
David Langmaid <drlangmaid@gmail.com>
2020-11-30 15:03:38 +0000

    filled out methods

--- Code/FrontEnd/WinScreenController.java
+++ Code/FrontEnd/WinScreenController.java
@@ -1,15 +1,27 @@
 package FrontEnd;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class WinScreenController {
+public class WinScreenController implements Initializable {
     @FXML
+    private Button returnButton;
+
+    @FXML
+    private Text winner;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        winner.setText("Congratulations" +  "!");
+    }
 
+    public void onReturnButton() {
+        WindowLoader wl = new WindowLoader(returnButton);
+        wl.load("MenuScreen");
     }
 }
